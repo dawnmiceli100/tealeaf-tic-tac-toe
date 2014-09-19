8de522c5082076795fdd76c66a828dede4b70c9e
Dawn Miceli <dawnmiceli100@yahoo.com>
2014-09-19 16:06:44 -0400

    consolidate two hashes to one

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -7,24 +7,25 @@ DIVIDER_ROW = "-----+-----+-----"
 
 board_results = { 1 => " ", 2 => " ", 3 => " ", 4 => " ", 5 => " ", 6 => " ", 7 => " ", 8 => " ", 9 => " "}
 
-game_board = {"top_section" => [[FILLER_ROW], ["  #{board_results[1]}  |  #{board_results[2]}  |  #{board_results[3]}  "], [FILLER_ROW], [DIVIDER_ROW]],
-              "middle_section" => [[FILLER_ROW], ["  #{board_results[4]}  |  #{board_results[5]}  |  #{board_results[6]}  "], [FILLER_ROW], [DIVIDER_ROW]], 
-              "bottom_section" => [[FILLER_ROW], ["  #{board_results[7]}  |  #{board_results[8]}  |  #{board_results[9]}  "], [FILLER_ROW]]}
-
-def display_board(board)
-  board.each do |key, value|
-    puts value
-  end 
+def display_board(results)
+  puts FILLER_ROW
+  puts "  #{results[1]}  |  #{results[2]}  |  #{results[3]}  "
+  puts FILLER_ROW
+  puts DIVIDER_ROW
+  puts FILLER_ROW
+  puts "  #{results[4]}  |  #{results[5]}  |  #{results[6]}  "
+  puts FILLER_ROW
+  puts DIVIDER_ROW
+  puts FILLER_ROW
+  puts "  #{results[7]}  |  #{results[8]}  |  #{results[9]}  "
+  puts FILLER_ROW
 end 
 
-display_board(game_board)
+display_board(board_results)
 puts "Welcome to Tic-Tac-Toe! You will be X and I will be O."
 puts "Choose a position from 1 to 9." 
 position = gets.chomp
 
 board_results[position.to_i] = "X"
-puts "board_results after update #{board_results}"
 
-puts game_board
-
-display_board(game_board)
+display_board(board_results)
