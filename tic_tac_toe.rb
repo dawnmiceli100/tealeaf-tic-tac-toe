d4f6fac7513311d2e258163bfc3a852d582e9ebc
Dawn Miceli <dawnmiceli100@yahoo.com>
2014-09-20 11:31:48 -0400

    finished game

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,11 +1,23 @@
 # tic-tac-toe.rb
 
-require 'pry'
-
 FILLER_ROW = "     |     |     "
 DIVIDER_ROW = "-----+-----+-----"
 
 board_results = { 1 => " ", 2 => " ", 3 => " ", 4 => " ", 5 => " ", 6 => " ", 7 => " ", 8 => " ", 9 => " "}
+
+def play_game(board_results)
+  begin
+    player_takes_turn(board_results)
+    game_over = check_game_over(board_results)
+    computer_takes_turn(board_results)
+  end until game_over
+  if game_over == "Tie" 
+    puts "The game ended in a tie."
+  else
+    puts "The game is over. The #{game_over} won."  
+  end
+  play_again?(board_results)
+end
 
 def display_board(results)
   puts FILLER_ROW
@@ -19,13 +31,56 @@ def display_board(results)
   puts FILLER_ROW
   puts "  #{results[7]}  |  #{results[8]}  |  #{results[9]}  "
   puts FILLER_ROW
+  puts " "
 end 
+
+def player_takes_turn(results)
+  puts "Choose a position from 1 to 9." 
+  position = gets.chomp
+  if results[position.to_i] == " "
+    results[position.to_i] = "X"
+    display_board(results)
+  else
+    puts "That position is already taken."
+    player_takes_turn(results) 
+  end   
+end  
+
+def computer_takes_turn(results)
+  a = results.keys.select { |key| results[key] == " "}
+  position = a.sample
+  results[position] = "O"
+  display_board(results)
+end 
+
+def check_game_over(results) 
+  three_in_a_row_possibilities = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]] 
+  three_in_a_row_possibilities.each do |row|
+    if results[row[0]] == "X" && results[row[1]] == "X" && results[row[2]] == "X"  
+      return "Player"
+    elsif results[row[0]] == "O" && results[row[1]] == "O" && results[row[2]] == "O"  
+      return "Computer"      
+    end 
+  end  
+  if results.keys.select { |key| results[key] == " "} == []
+    return "Tie" 
+  end
+  nil
+end  
+
+def play_again?(board_results)
+  puts "Play again? (Y or N)" 
+  if gets.chomp.downcase == "y"
+    reset_board(board_results) 
+    play_game(board_results)
+  end
+end     
+
+def reset_board(results)
+  results.each_key {|key| results[key] = " "}
+  display_board(results)
+end  
 
 display_board(board_results)
 puts "Welcome to Tic-Tac-Toe! You will be X and I will be O."
-puts "Choose a position from 1 to 9." 
-position = gets.chomp
-
-board_results[position.to_i] = "X"
-
-display_board(board_results)
+play_game(board_results)
