eae52d0dcb3a697e3bf7144a45b3893e34eee090
Dawn Miceli <dawnmiceli100@yahoo.com>
2014-09-20 15:39:27 -0400

    Made changes to clear screen before each display; Check for win after each player and computer turn.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -9,7 +9,12 @@ def play_game(board_results)
   begin
     player_takes_turn(board_results)
     game_over = check_game_over(board_results)
-    computer_takes_turn(board_results)
+    if game_over
+      break
+    else  
+      computer_takes_turn(board_results)
+      game_over = check_game_over(board_results)
+    end  
   end until game_over
   if game_over == "Tie" 
     puts "The game ended in a tie."
@@ -20,6 +25,7 @@ def play_game(board_results)
 end
 
 def display_board(results)
+  system 'clear'
   puts FILLER_ROW
   puts "  #{results[1]}  |  #{results[2]}  |  #{results[3]}  "
   puts FILLER_ROW
@@ -59,7 +65,7 @@ def check_game_over(results)
     if results[row[0]] == "X" && results[row[1]] == "X" && results[row[2]] == "X"  
       return "Player"
     elsif results[row[0]] == "O" && results[row[1]] == "O" && results[row[2]] == "O"  
-      return "Computer"      
+      return "Computer"  
     end 
   end  
   if results.keys.select { |key| results[key] == " "} == []
